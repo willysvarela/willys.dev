5c88487ba95b8b224b7ca6d34754fcb24126e36a
willysv <willysv@motorola.com>
2022-03-09 18:43:11 -0400

    Style home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
+import { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
+
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+    const typeEl = useRef<HTMLElement>(null);
+    const typed = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        const options = {
+            strings: ['Oi!^500 eu sou o Willys^1000 :)'],
+            typeSpeed: 50,
+            backSpeed: 50
+        };
+        typed.current = new Typed(typeEl.current, options);
+    }, []);
+
     return (
         <div className={styles.container}>
             <Head>
@@ -12,13 +27,26 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>
-                <div>Oi, eu sou o Willys</div>
+            <main className={styles.main}>
+                <div className={styles.title}>
+                    <span ref={typeEl}></span>
+                </div>
+                <br/>
                 <div className={styles.links}>
-                    <a target="_blank" href="https://www.linkedin.com/in/willysvarela" rel="noreferrer">linkedin</a>
-                    <a target="_blank" href="https://www.github.com/willysvarela" rel="noreferrer">github</a>
-                    <a target="_blank" href="https://www.twitter.com/willysvarela" rel="noreferrer">twitter</a>
-                    <a href="/daily">daily</a>
+                    <a
+                        target="_blank"
+                        href="https://www.linkedin.com/in/willysvarela"
+                        rel="noreferrer"
+                    >
+                        linkedin
+                    </a>
+                    <a target="_blank" href="https://www.github.com/willysvarela" rel="noreferrer">
+                        github
+                    </a>
+                    <a target="_blank" href="https://www.twitter.com/willysvarela" rel="noreferrer">
+                        twitter
+                    </a>
+                    <a href="/daily">Mini-Projetos</a>
                     <a href="/blog">blog</a>
                 </div>
             </main>
